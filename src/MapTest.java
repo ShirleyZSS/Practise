77bd6ff6113d3d1ae1f7f6b7832395a95d94e8cf
””shirley <Shirley_ZSS0093@163.com>
2017-08-14 10:45:04 +0800

    用entrySet方法迭代map集合

---
+++ src/MapTest.java
@@ -0,0 +1,23 @@
+import javax.xml.ws.EndpointReference;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Shirley on 2017/8/14.
+ */
+public class MapTest {
+    public static void main(String args[]) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("1","赵姗姗");
+        map.put("2","范媛媛");
+        map.put("3","陈晓丽");
+        Set<Map.Entry<String,String>> entrySet=map.entrySet();
+        Iterator<Map.Entry<String ,String >> it=entrySet.iterator();
+        while (it.hasNext()){
+            Map.Entry<String ,String > entry=it.next();
+            System.out.println("键："+entry.getKey()+"值："+entry.getValue());
+        }
+    }
+}
