6b579392fa27887a7f27d57860d638d13d27525f
””shirley <Shirley_ZSS0093@163.com>
2017-08-23 09:08:30 +0800

    完善版：切割&合并mp3(借助SequenceInputStream）

--- src/SequenceInputStreamTest.java
+++ src/SequenceInputStreamTest.java
@@ -13,8 +13,9 @@ public class SequenceInputStreamTest {
         //将文件分割成5份
         File file=new File("C:\\Users\\25094\\Music\\薛之谦 - 演员.mp3");
         FileInputStream fileInputStream=new FileInputStream(file);
+        File dir=new File("C:\\Users\\25094\\Desktop\\切割");
 
-        File file1=new File("C:\\Users\\25094\\Desktop\\part1.mp3");
+       /* File file1=new File("C:\\Users\\25094\\Desktop\\part1.mp3");
         FileOutputStream fileOutputStream1=new FileOutputStream(file1);
         File file2=new File("C:\\Users\\25094\\Desktop\\part2.mp3");
         FileOutputStream fileOutputStream2=new FileOutputStream(file2);
@@ -24,42 +25,46 @@ public class SequenceInputStreamTest {
         FileOutputStream fileOutputStream4=new FileOutputStream(file4);
         File file5=new File("C:\\Users\\25094\\Desktop\\part5.mp3");
         FileOutputStream fileOutputStream5=new FileOutputStream(file5);
+*/
 
-        ArrayList<FileOutputStream> list=new ArrayList<>();
+        /*ArrayList<FileOutputStream> list=new ArrayList<>();
         list.add(fileOutputStream1);
         list.add(fileOutputStream2);
         list.add(fileOutputStream3);
         list.add(fileOutputStream4);
-        list.add(fileOutputStream5);
+        list.add(fileOutputStream5);*/
+
 
         byte[] buf=new byte[1024*1024];
         int length=0;
-        int i=0;
-        while((length=fileInputStream.read(buf))!=-1){
-            list.get(i).write(buf,0,length);
-            i++;
+        //int i=0;
+        for(int i=0;(length=fileInputStream.read(buf))!=-1;i++){
+           /* list.get(i).write(buf,0,length);
+            i++;*/
+          FileOutputStream fileOutputStream= new FileOutputStream(new File(dir,"part"+i+".mp3"));
+          fileOutputStream.write(buf,0,length);
+          fileOutputStream.close();
         }
-        fileOutputStream5.close();
+       /* fileOutputStream5.close();
         fileOutputStream4.close();
         fileOutputStream3.close();
         fileOutputStream2.close();
         fileOutputStream1.close();
-        fileInputStream.close();
+        fileInputStream.close();*/
 
         //将分割后的文件再合成一份
-        FileInputStream fileInputStream1=new FileInputStream(file1);
+       /* FileInputStream fileInputStream1=new FileInputStream(file1);
         FileInputStream fileInputStream2=new FileInputStream(file2);
         FileInputStream fileInputStream3=new FileInputStream(file3);
         FileInputStream fileInputStream4=new FileInputStream(file4);
-        FileInputStream fileInputStream5=new FileInputStream(file5);
+        FileInputStream fileInputStream5=new FileInputStream(file5);*/
+       File[] files=dir.listFiles();
+        Vector<FileInputStream> vector=new Vector<>();
+       for(File file1:files){
+           vector.addElement(new FileInputStream(file1));
+       }
         File sumFile=new File("C:\\Users\\25094\\Desktop\\演员.mp3");
         FileOutputStream fileOutputStream=new FileOutputStream(sumFile);
-        Vector<FileInputStream> vector=new Vector<>();
-        vector.addElement(fileInputStream1);
-        vector.addElement(fileInputStream2);
-        vector.addElement(fileInputStream3);
-        vector.addElement(fileInputStream4);
-        vector.addElement(fileInputStream5);
         Enumeration<FileInputStream> e=vector.elements();
         SequenceInputStream sequenceInputStream=new SequenceInputStream(e);
         byte[] buf1=new byte[1024];
@@ -68,11 +73,12 @@ public class SequenceInputStreamTest {
             fileOutputStream.write(buf1,0 ,content);
         }
         fileOutputStream.close();
-        fileInputStream5.close();
+        sequenceInputStream.close();
+      /*  fileInputStream5.close();
         fileInputStream4.close();
         fileInputStream3.close();
         fileInputStream2.close();
-        fileInputStream1.close();
+        fileInputStream1.close();*/
 
 
 
