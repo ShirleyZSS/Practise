5de6c12e5ddcdccb2e9c16dab35a65de93099a29
””shirley <Shirley_ZSS0093@163.com>
2017-09-01 08:52:01 +0800

    static修饰成员变量 小练习

--- src/StaticTest.java
+++ src/StaticTest.java
@@ -1,26 +1,36 @@
 /**
  * Created by Shirley on 2017/8/31.
+ * 记录该类被创建了多少个对象
  */
 class Student{
     String name;
     String sex;
     static int count=0;
+    {
+        count++;//构造代码块
+    }
 
     public Student(String name,String sex){
         this.name=name;
         this.sex=sex;
-        count++;
     }
+
+    public Student(){
+
+    }
+
 }
 
 public class StaticTest {
 
-    public static void main(String args){
+    public static void main(String args[]){
         Student s1=new Student("赵姗姗","女" );
         System.out.println(Student.count);
         Student s2=new Student("赵姗姗","女" );
         System.out.println(Student.count);
         Student s3=new Student("赵姗姗","女" );
+        System.out.println(Student.count);
+        Student s4=new Student();
         System.out.println(Student.count);
 
     }
